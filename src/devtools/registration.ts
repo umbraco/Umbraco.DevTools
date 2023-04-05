c6cbb33e561b3714e35e35cdd6be44f1a03a18c6
Warren Buckley <warren@umbraco.com>
2023-04-05 16:28:17 +0100

    Make sure the registration script only loads the devtools panel if there is an <umb-app> on the page

--- src/devtools/registration.ts
+++ src/devtools/registration.ts
@@ -1,14 +1,23 @@
 import browser from "webextension-polyfill";
 
-browser.devtools.panels.elements.createSidebarPane("Umbraco Warren").then((sidebar) => {
-    browser.devtools.panels.elements.onSelectionChanged.addListener(() => {
-        sidebar.setExpression('$0');
-    });
-});
-
 // Check if we can find the <umb-app> element in the page
-// We check the length rather than getting the DOM element itself as it can be serialized
+// We check the length rather than getting the DOM element itself as it can NOT be serialized
 // as JSON over the DevTools protocol. So hence a simple check for the length of the array
 browser.devtools.inspectedWindow.eval("document.getElementsByTagName('umb-app').length").then((result) => {
-    console.log('RESULT WARREN', result);
+    
+    // We only expect the result of the JS call above to be 0 or 1
+    // The result is an array of values that contains the result of the JS call and the other is any errors
+    const valueOfEval = result[0];
+
+    // As the value is 0 or 1 lets be lazy and use this like a boolean
+    if(valueOfEval) {
+        browser.devtools.panels.elements.createSidebarPane("Umbraco").then((sidebar) => {
+            sidebar.setPage("panel.html");
+        });
+    } else {
+        console.warn("Did not find the <umb-app> element in the page, so the Umbraco DevTools panel will not be shown.");
+    }
+
+}).catch((err) => {
+    console.error('err', err);
 });
